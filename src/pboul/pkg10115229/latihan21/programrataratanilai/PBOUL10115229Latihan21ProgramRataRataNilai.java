852c89f374797a5b72a6ba1011ecd5aa66b63aba
fakhri ramdani <muhammadlukman09@gmail.com>
2019-10-10 21:13:17 +0700

    Project Keduasatu

---
+++ src/pboul/pkg10115229/latihan21/programrataratanilai/PBOUL10115229Latihan21ProgramRataRataNilai.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboul.pkg10115229.latihan21.programrataratanilai;
+
+/**
+ *
+ * @author Lukman
+ * NIM      : 10115229
+ * NAMA     : MUHAMMAD LUKMAN HAKIM
+ * KELAS    : PBO ULANG
+ */
+
+import java.util.Scanner;
+
+public class PBOUL10115229Latihan21ProgramRataRataNilai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+    int array[], i, n;
+    array = new int[100];
+    float rata, total=0;
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Masukkan Banyaknya Mahasiswa: ");
+    n = scan.nextInt();
+    for(i = 1; i <= n; i++){
+      System.out.print("Nilai Mahasiswa Ke-"+ i +" : ");
+      array[i] = scan.nextInt();
+      total = total + array[i];
+    }
+    rata = total/n;
+    System.out.println("Hasil nilai total adalah : " + total);
+    System.out.println("Hasil rata-rata adalah : " + rata);
+    
+    System.out.println("");
+    System.out.println("  develop BY: Muhammad Lukman Hakim ");
+
+  }
+}
+    
+
